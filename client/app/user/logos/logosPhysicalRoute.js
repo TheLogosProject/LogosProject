378ad74a9127b4ff1e9e3a70dff316eef533c4b9
mguenther006 <mguenther006@users.noreply.github.com>
2015-09-15 13:15:44 -0600

    Added logosPhysical stage and route
    -Added logosPhysical stage and route
    -Added logosPhysicalRoute.js to index.html
    
    Signed-off-by: mguenther006 <mguenther006@users.noreply.github.com>

---
+++ client/app/user/logos/logosPhysicalRoute.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('app')
+  .config(function ($stateProvider) {
+    $stateProvider
+    .state('logosPhysical', {
+      url: '/logosPhysical',
+      // will need to tie this to username for unique view for each member
+      templateUrl: 'app/user/logos/logosPhysical.html'
+    });
+  });
