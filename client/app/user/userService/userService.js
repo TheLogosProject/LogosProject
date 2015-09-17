2c35177ca5e31c99d2a02e31fe0ac598b15bfaf9
Brian DAmore <brian.damore@gmail.com>
2015-09-17 14:58:21 -0600

    commented out lot of back end stuff on user
    - did this to make user landing page work
    
    Signed-off-by: Brian DAmore <brian.damore@gmail.com>

--- client/app/user/userService/userService.js
+++ client/app/user/userService/userService.js
@@ -1,13 +1,13 @@
 app.service('userService', function ($http) {
 
-    this.getUserByID = function () {
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:8555/api/user/55fafb27a643ff22616fe33c'
-        })
-            .then(function (response) {
-                return response.data;
-            });
-    };
+    // this.getUserByID = function () {
+    //     return $http({
+    //         method: 'GET',
+    //         url: 'http://localhost:8555/api/user/55fafb27a643ff22616fe33c'
+    //     })
+    //         .then(function (response) {
+    //             return response.data;
+    //         });
+    // };
 
 });
