7127491e6c3a85920c1191331bce56b9d9891fa0
Brian Ng <bcn220@gmail.com>
2015-09-18 11:57:22 -0600

    add redirect on successful gym creation

--- client/app/admin/gym/main/gymCreate/gymCreateCtrl.js
+++ client/app/admin/gym/main/gymCreate/gymCreateCtrl.js
@@ -2,7 +2,8 @@ app.controller('gymCreateCtrl', function ($scope, gymCreateService, $location) {
     $scope.addGym = function (gymInfo) {
         gymCreateService.createGym(gymInfo).then(function (response) {
             $scope.gym.name = '';
-            Materialize.toast('Gym added successfully', 3000);
+            $location.path('/gym/main');
+            Materialize.toast('Gym added successfully', 5000);
         }, function (err) {
           Materialize.toast('There was error.  Please try again.', 3000);
         });
--- client/app/admin/gym/main/gymLanding/gymLandingCtrl.js
+++ client/app/admin/gym/main/gymLanding/gymLandingCtrl.js
@@ -1,7 +1,6 @@
 app.controller('gymLandingCtrl', function ($scope, gymLandingService) {
     $scope.getGym = function () {
         gymLandingService.getGym().then(function(response) {
-          console.log(response);
           for (var i = 0; i < response.length; i++) {
             if (response[i]["active"] === true) {
               response[i]["active"] = "Yes";
