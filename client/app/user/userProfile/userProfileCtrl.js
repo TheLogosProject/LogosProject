8f795dee57b132b5120668597b2c6c4e61c92270
mguenther006 <mguenther006@users.noreply.github.com>
2015-09-18 15:27:47 -0600

    userProfile update functionality
    -building userProfile update functionality
    
    Signed-off-by: mguenther006 <mguenther006@users.noreply.github.com>

--- client/app/user/userProfile/userProfileCtrl.js
+++ client/app/user/userProfile/userProfileCtrl.js
@@ -6,21 +6,35 @@ app.controller('userProfileCtrl', function ($scope, $stateParams, userProfileSvc
   var getProfile = function (memberId) {
     userProfileSvc.getUser(memberId).then(function (response) {
       console.log(response);
-      $scope.userInfo = response;
+      $scope.userInfo = {
+        _id: response._id,
+        firstName: response.user.name.first,
+        lastName: response.user.name.last,
+        email: response.user.contact_info.email,
+        phone: response.user.contact_info.phone,
+        gender: response.user.gender,
+        heightFeet: response.user.height.feet,
+        heightInches: response.user.height.inches,
+        weight: response.user.weight,
+        goals: response.user.goals,
+        gym: response.gym.name
+      };
+      $scope.updateProfile = function(userInfo){
+        userProfileSvc.updateUserData(userInfo).then(function (response) {
+          getProfile();
+          $location.path('/userLanding');
+          Materialize.toast('Updated successfully', 5000);
+        }, function (err) {
+          Materialize.toast('There was an error', 3000);
+        });
+        console.log(userInfo);
+      };
     });
   };
   getProfile(memberId);
 
-  //Update user profile information
-  $scope.updateProfile = function (userData) {
-    userProfileSvc.updateUserData(user).then(function (response) {
-      getProfile();
-      $location.path('/userLanding');
-      Materialize.toast('Updated successfully', 1000);
-    }, function (err) {
-      Materialize.toast('There was an error', 1000);
-    });
-  };
 
 
+
+  
 });
--- client/app/user/userProfile/userProfileSvc.js
+++ client/app/user/userProfile/userProfileSvc.js
@@ -1,32 +1,25 @@
-app.service('userProfileSvc', function ($http, $q) {
+app.service('userProfileSvc', function ($http, $stateParams) {
 
-//get user profile information
-    this.getUser = function (memberId) {
-      var deferred = $q.defer();
-      $http({
-        method: 'GET',
-        url: 'http://localhost:8555/api/user-details/' + memberId,
-      }).then(function (response) {
-        deferred.resolve(response.data);
-      });
-      return deferred.promise;
-    };
+  //get user profile information
+  this.getUser = function (memberId) {
+    return $http({
+      method: 'GET',
+      url: 'http://localhost:8555/api/user-details/' + memberId,
+    }).then(function (response) {
+      return response.data;
+    });
+  };
 
-//service for updating user profile
-    this.updateUserData = function (userData) {
-      var deferred = $q.defer();
-      $http({
-        method: 'PUT',
-        url: 'http://localhost:1337/api/user/' + user._id,
-        data: {
-          //edit this for the form
-          userFirstName: user.firstName
-        }
-      }).then(function (response) {
-        deferred.resolve(response);
-      });
-      return deferred.promise;
-    };
+  //service for updating user profile
+  this.updateUserData = function (userData) {
+    return $http({
+      method: 'PUT',
+      url: 'http://localhost:1337/api/user-update/' + userData._id,
+      data: userData
+    }).then(function (response) {
+      return response.data;
+    });
+  };
 
 
 });
