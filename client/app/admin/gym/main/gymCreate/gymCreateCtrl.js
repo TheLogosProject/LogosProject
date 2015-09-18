53bc4b44e658510d2c0f153d3a4ca2e042ab374b
Brian Ng <bcn220@gmail.com>
2015-09-18 10:07:26 -0600

    add success/error message on gym creation

--- client/app/admin/gym/main/gymCreate/gymCreateCtrl.js
+++ client/app/admin/gym/main/gymCreate/gymCreateCtrl.js
@@ -1,7 +1,10 @@
-app.controller('gymCreateCtrl', function ($scope, gymCreateService) {
+app.controller('gymCreateCtrl', function ($scope, gymCreateService, $location) {
     $scope.addGym = function (gymInfo) {
         gymCreateService.createGym(gymInfo).then(function (response) {
-            return response;
+            $scope.gym.name = '';
+            Materialize.toast('Gym added successfully', 3000);
+        }, function (err) {
+          Materialize.toast('There was error.  Please try again.', 3000);
         });
     };
 });
