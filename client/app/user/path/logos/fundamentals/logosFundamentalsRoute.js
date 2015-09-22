0d95e843d034533d3f6d74fadbcbafb0b93cd424
Brian Ng <bcn220@gmail.com>
2015-09-22 00:12:58 -0600

    remove nested routing on logosKnowledgeRoutes.js

--- client/app/user/path/logos/fundamentals/logosFundamentalsRoute.js
+++ client/app/user/path/logos/fundamentals/logosFundamentalsRoute.js
@@ -3,8 +3,8 @@
 angular.module('app')
   .config(function ($stateProvider) {
     $stateProvider
-    .state('logos.fundamentals', {
-      url: '/fundamentals',
+    .state('logosFundamentals', {
+      url: '/path/logos/fundamentals',
       // will need to tie this to username for unique view for each member
       templateUrl: 'app/user/path/logos/fundamentals/logosFundamentals.html'
     });
