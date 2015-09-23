d6a0810138ee87c2c686674f9d9485b732e2d700
Brian DAmore <brian.damore@gmail.com>
2015-09-23 17:00:21 -0600

    -fixed manage gyms endpoints
    -still need to programming and coaches with mike
    
    Signed-off-by: Brian DAmore <brian.damore@gmail.com>

--- server/api/gym/index.js
+++ server/api/gym/index.js
@@ -9,18 +9,18 @@ var config = require('../../config/environment');
 
 var router = express.Router();
 
-
-router.get('/api/gym', gymsCtrl.getAllGyms);
-router.get('/api/gym-names', gymsCtrl.getGymNames);
-router.get('/api/gym-details/:gymId', gymsCtrl.getGymDetails);
-router.get('/api/gym-pathway/:gymId', gymsCtrl.getGymPathway);//PROGRAMMING/GET GYM
-router.post('/api/gym-stage', gymsCtrl.getStages);//PROGRAMMING/GET STAGES
-router.post('/api/gym-evaluations', gymsCtrl.getEvaluations);//PROGRAMMING/GET EVALUATIONS
-router.post('/api/gym-evaluation-specifics', gymsCtrl.getSpecificEval);//PROGRAMMING/GET SPECIFIC EVALUATION
-router.post('/api/gym', gymsCtrl.saveGym);
-router.post('/api/gym-add-evaluation', gymsCtrl.addEvaluation);//PROGRAMMING/ADDING EVAL
-router.put('/api/gym-update', gymsCtrl.updateGym);
-router.put('/api/gym-edit-evaluation', gymsCtrl.editEvaluation);//PROGRAMMING/EDIT EVAL
+//think of these all as '/gym' + 'xxxxxx'
+router.get('/', gymsCtrl.getAllGyms);
+router.get('/names', gymsCtrl.getGymNames);
+router.get('/details/:gymId', gymsCtrl.getGymDetails);
+router.get('/pathway/:gymId', gymsCtrl.getGymPathway);//PROGRAMMING/GET GYM
+router.post('/stage', gymsCtrl.getStages);//PROGRAMMING/GET STAGES
+router.post('/evaluations', gymsCtrl.getEvaluations);//PROGRAMMING/GET EVALUATIONS
+router.post('/evaluation-specifics', gymsCtrl.getSpecificEval);//PROGRAMMING/GET SPECIFIC EVALUATION
+router.post('/', gymsCtrl.saveGym);
+router.post('/add-evaluation', gymsCtrl.addEvaluation);//PROGRAMMING/ADDING EVAL
+router.put('/update', gymsCtrl.updateGym);
+router.put('/edit-evaluation', gymsCtrl.editEvaluation);//PROGRAMMING/EDIT EVAL
 
 module.exports = router;
 
