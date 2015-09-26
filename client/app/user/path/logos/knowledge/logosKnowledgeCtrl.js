ee8e59efb203ec09f31e649210c77f39bf57bce2
mikebuckley5 <que.onda.buckley@gmail.com>
2015-09-26 01:46:33 -0600

    connected enpoints for knowlege path

--- client/app/user/path/logos/knowledge/logosKnowledgeCtrl.js
+++ client/app/user/path/logos/knowledge/logosKnowledgeCtrl.js
@@ -1,33 +1,26 @@
-app.controller('logosKnowledgeCtrl', function ($scope, $stateParams, Auth, profileSvc) {
-
+app.controller('logosKnowledgeCtrl', function ($scope, $stateParams, $timeout, Auth, logosKnowledgeService) {
 
     var currentUser = Auth.getCurrentUser();
     $scope.knowledgeEvaluations = currentUser.pathways[0].stages[1].evaluations;
     var pathwayID = currentUser.pathways[0]._id;
     var stageID = currentUser.pathways[0].stages[1]._id;
     $scope.evaluationsID = currentUser.pathways[0].stages[1].evaluations;
-    // $scope.answer = currentUser.pathways[0].stages[1].evaluationsID.content.answer;
+    $scope.logosPercent = currentUser.pathways[0].completion.amount_completed;
 
     $scope.knowledgeInfo = {
-      userID: currentUser._id,
-      pathwayID: pathwayID,
-      stageID: stageID
+        userID: currentUser._id,
+        pathwayID: pathwayID,
+        stageID: stageID
     };
 
     $scope.submitKnowledge = function (obj) {
-      $scope.knowledgeInfo.answer = obj.answer;
+        $scope.knowledgeInfo.answer = obj.content.answer;
         $scope.knowledgeInfo.evalID = obj._id;
-        console.log($scope.knowledgeInfo);
-        profileSvc.updateUserData($scope.knowledgeInfo).then(function (response) {
-            Materialize.toast('Updated successfully', 5000);
+        logosKnowledgeService.updateUserData($scope.knowledgeInfo).then(function (response) {
+            Materialize.toast('Updated successfully', 1500);
+            $timeout(function () { document.location.reload(true); }, 1500);
         }, function (err) {
             Materialize.toast('There was an error', 3000);
         });
     };
-
-
-    // $scope.user = userObj;
-    // $scope.logosPercent = Math.ceil($scope.user.pathways.logos.completion.amount_completed);
-    // $scope.pathosPercent = Math.ceil($scope.user.pathways.pathos.completion.amount_completed);
-    // $scope.ethosPercent = Math.ceil($scope.user.pathways.ethos.completion.amount_completed);
 });
