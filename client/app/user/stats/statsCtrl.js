9bd64847ba52f50205a54deb5c130ca456f96059
mguenther006 <mguenther006@users.noreply.github.com>
2015-09-26 03:11:49 -0600

    Populated current info in Stats.html
    -Populated current information from user object into Stats.html
    -Show badhes when path has been completed
    
    Signed-off-by: mguenther006 <mguenther006@users.noreply.github.com>

--- client/app/user/stats/statsCtrl.js
+++ client/app/user/stats/statsCtrl.js
@@ -19,6 +19,18 @@
       $scope.isAdmin = Auth.isAdmin;
       $scope.getCurrentUser = Auth.getCurrentUser;
 
+
+//to display logos badge
+      $scope.logosBadge = $scope.getCurrentUser().pathways[0].completion.complete;
+
+
+//for pathos badge when ready
+      // $scope.pathosBadge = $scope.getCurrentUser().pathways[1].completion.complete;
+
+
+//for ethos badge when ready
+      // $scope.ethosBadge = $scope.getCurrentUser().pathways[2].completion.complete;
+
       console.log($scope.getCurrentUser());
 
       $scope.logout = function () {
