c5a96215fea5e5a7a6f3ce0e94daa18fbc6a754d
mguenther006 <mguenther006@users.noreply.github.com>
2015-09-25 20:54:35 -0600

    Logos Fundamentals
    -Added logic to logosFundamentals to show/hide based on users status
    -Live data is brought in via JSON object
    -Created custom filter to translate text from JSON object to HTML
    
    Signed-off-by: mguenther006 <mguenther006@users.noreply.github.com>

--- client/app/user/path/logos/fundamentals/logosFundamentalsCtrl.js
+++ client/app/user/path/logos/fundamentals/logosFundamentalsCtrl.js
@@ -1,10 +1,19 @@
-app.controller('logosFundamentalsCtrl', function ($scope, $stateParams, Auth) {
+app.controller('logosFundamentalsCtrl', function ($scope, $stateParams, $sce, Auth) {
 
+//auth information
   var that = this;
   that.isAdmin = Auth.isAdmin;
 
   $scope.getCurrentUser = Auth.getCurrentUser;
 
   $scope.user = $scope.getCurrentUser();
-      $scope.logosPercent = Math.ceil($scope.user.pathways[0]["completion"]["amount_completed"]);
+  $scope.logosPercent = Math.ceil($scope.user.pathways[0]["completion"]["amount_completed"]);
+
+//information to ng-repeat the lists//
+  $scope.movements = $scope.user.pathways[0].stages[0].evaluations;
+
+//unlock appropriate tabs//
+  $scope.step1 = $scope.user.pathways[0].stages[0].complete;
+  $scope.step2 = $scope.user.pathways[0].stages[1].complete;
+
 });
