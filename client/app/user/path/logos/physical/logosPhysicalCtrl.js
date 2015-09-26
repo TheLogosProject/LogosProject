d9f0c1a1c3b059cece549ded82f2dcf45340ec05
Brian Ng <bcn220@gmail.com>
2015-09-25 22:11:32 -0600

    ng repeat physical content thru page
    - update navbar to membersLanding

--- client/app/user/path/logos/physical/logosPhysicalCtrl.js
+++ client/app/user/path/logos/physical/logosPhysicalCtrl.js
@@ -1,9 +1,11 @@
 app.controller('logosPhysicalCtrl', function ($scope, $stateParams, Auth) {
 
     $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.user = $scope.getCurrentUser();
 
-    $scope.physicalEvaluations = $scope.getCurrentUser().pathways[0].stages[2].evaluations;
+    $scope.movements = $scope.user.pathways[0].stages[2].evaluations;
 
+    console.log($scope.user.pathways[0].stages[2].evaluations[0].content.explanation);
     // $scope.user = userObj;
     // $scope.logosPercent = Math.ceil($scope.user.pathways.logos.completion.amount_completed);
     // $scope.pathosPercent = Math.ceil($scope.user.pathways.pathos.completion.amount_completed);
