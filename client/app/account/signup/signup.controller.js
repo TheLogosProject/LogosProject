8e29daf9d4ebd44d8924f9b75d6a9913c98ab85d
mguenther006 <mguenther006@users.noreply.github.com>
2015-09-28 14:03:01 -0600

    Added ng-click to register grabbing user info
    -Added ng-click to register button to grab user info after form is submitted
    -Also added the get current use info to self-invoke at the landing page to grab the newest user information.
    
    Signed-off-by: mguenther006 <mguenther006@users.noreply.github.com>

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -1,70 +1,73 @@
 (function () {
   'use strict';
 
-angular.module('app')
-  .controller('SignupCtrl', function ($scope, Auth, $location, $window, $http, gyms) {
+  angular.module('app')
+    .controller('SignupCtrl', function ($scope, Auth, $location, $window, $http, gyms) {
 
-    //
-    $scope.user = {};
-    $scope.errors = {};
+      //
+      $scope.user = {};
+      $scope.errors = {};
 
-    //Drop Down
-    var gymsArr = [];
-    for (var i = 0; i < gyms.length; i++) {
+      $scope.getUser = function () {
+        Auth.getCurrentUser();
+};
+
+      //Drop Down
+      var gymsArr = [];
+      for (var i = 0; i < gyms.length; i++) {
         var gymInfo = {
-            label: gyms[i]["name"],
-            value: gyms[i]["_id"]
+          label: gyms[i]["name"],
+          value: gyms[i]["_id"]
         };
         gymsArr.push(gymInfo);
-    }
-    $scope.gymOptions = gymsArr;
-
-
-    //Auth + User
-    $scope.register = function(form) {
-      $scope.submitted = true;
-      if(form.$valid) {
-        Auth.createUser({
-          name: {
-            first: $scope.user.name.first,
-            last: $scope.user.name.last
-          },
-          email: $scope.user.email,
-          password: $scope.user.password,
-          gym: $scope.user.gym
-
-          // team: $scope.user.team
-        })
-        .then( function() {
-          $location.path('/main');
-        })
-        .catch( function(err) {
-          err = err.data;
-          $scope.errors = {};
-
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err.errors, function(error, field) {
-            form[field].$setValidity('mongoose', false);
-            $scope.errors[field] = error.message;
-          });
-        });
       }
-    };
+      $scope.gymOptions = gymsArr;
 
-    $scope.loginOauth = function(provider) {
-      $window.location.href = '/auth/' + provider;
-    };
-  });
+
+      //Auth + User
+      $scope.register = function (form) {
+        $scope.submitted = true;
+        if (form.$valid) {
+          Auth.createUser({
+              name: {
+                first: $scope.user.name.first,
+                last: $scope.user.name.last
+              },
+              email: $scope.user.email,
+              password: $scope.user.password,
+              gym: $scope.user.gym
+
+            })
+            .then(function () {
+              $location.path('/main');
+            })
+            .catch(function (err) {
+              err = err.data;
+              $scope.errors = {};
+
+              // Update validity of form fields that match the mongoose errors
+              angular.forEach(err.errors, function (error, field) {
+                form[field].$setValidity('mongoose', false);
+                $scope.errors[field] = error.message;
+              });
+            });
+        }
+      };
+
+      $scope.loginOauth = function (provider) {
+        $window.location.href = '/auth/' + provider;
+      };
+    });
 
 }());
 
 // bring in gym array for gym dropdown
-            // var gymsArr = [];
-            // for (var i = 0; i < gyms.length; i++) {
-            //     var gymInfo = {
-            //         label: gyms[i]["name"],
-            //         value: gyms[i]["_id"]
-            //     };
-            //     gymsArr.push(gymInfo);
-            // }
-            // $scope.gymOptions = gymsArr;
+// var gymsArr = [];
+// for (var i = 0; i < gyms.length; i++) {
+//     var gymInfo = {
+//         label: gyms[i]["name"],
+//         value: gyms[i]["_id"]
+//     };
+//     gymsArr.push(gymInfo);
+// }
+// $scope.gymOptions = gymsArr;
