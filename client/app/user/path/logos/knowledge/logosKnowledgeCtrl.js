a9f8e4461114d7537cb37aaedbd02ea14c386598
Brian Ng <bcn220@gmail.com>
2015-09-29 16:29:13 -0600

    update step function code on knowledge page

--- client/app/user/path/logos/knowledge/logosKnowledgeCtrl.js
+++ client/app/user/path/logos/knowledge/logosKnowledgeCtrl.js
@@ -1,12 +1,22 @@
 app.controller('logosKnowledgeCtrl', function ($scope, $stateParams, $timeout, Auth, logosKnowledgeService) {
 
     var currentUser = Auth.getCurrentUser();
+
+    // bring logos knowledge questions onto scope
     $scope.knowledgeEvaluations = currentUser.pathways[0].stages[1].evaluations;
-    var pathwayID = currentUser.pathways[0]._id;
-    var stageID = currentUser.pathways[0].stages[1]._id;
-    $scope.evaluationsID = currentUser.pathways[0].stages[1].evaluations;
+
+    // bring percentageComplete # into scope for circle
     $scope.logosPercent = Math.ceil(currentUser.pathways[0].completion.amount_completed);
 
+    //unlock appropriate tabs
+    $scope.step1 = currentUser.pathways[0].stages[0].complete;
+    $scope.step2 = currentUser.pathways[0].stages[1].complete;
+
+    // set up variables to route knowledge answer to write endpoint
+    var pathwayID = currentUser.pathways[0]._id;
+    var stageID = currentUser.pathways[0].stages[1]._id;
+
+    // create knowledgeInfo object to send to endpoint
     $scope.knowledgeInfo = {
         userID: currentUser._id,
         pathwayID: pathwayID,
