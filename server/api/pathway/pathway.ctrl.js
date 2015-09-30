fcbed07e362ab805968830b6ac2cece5b4a0b6d9
mikebuckley5 <que.onda.buckley@gmail.com>
2015-09-30 10:58:16 -0600

    began commenting code

--- server/api/pathway/pathway.ctrl.js
+++ server/api/pathway/pathway.ctrl.js
@@ -1,6 +1,9 @@
 var Pathway = require('./pathway.model');
 
 module.exports = {
+    ///////////////////////////
+    /////GET ALL PATHWAYS/////
+    /////////////////////////
     find: function (req, res) {
         Pathway.find(req.query)
             .exec(function (err, response) {
@@ -11,6 +14,9 @@ module.exports = {
                 }
             });
     },
+    ////////////////////////////
+    /////SAVE THE PATHWAYS/////
+    //////////////////////////
     save: function (req, res) {
         var newPathway = new Pathway(req.body);
         newPathway.save(function (err, response) {
@@ -22,55 +28,3 @@ module.exports = {
         });
     }
 };
-
-////DELETE ONCE PUSHED LIVE////
-
-//For testing with Postman, please don't change this info!
-/*
-{
-    "pathway": [
-        {
-            "name": "Logos",
-            "completion": {
-                "amount_completed": 0,
-                "total_to_complete": 100,
-                "complete": false
-            },
-            "stages": [
-                {
-                    "name": "Stage 1",
-                    "amount_completed": 0,
-                    "total_to_complete": 0,
-                    "complete": false,
-                    "evaluations": [
-                        {
-                            "name": "Excersize 1",
-                            "content": {
-                                "video": "https://www.youtube.com/watch?v=8A6Uai5sQVw",
-                                "image": "http://www.70sbig.com/wp-content/uploads/2012/03/halfsquat-fuck.jpg",
-                                "progressions": [
-                                    {
-                                        "explanation": "Run 100 yards.",
-                                        "complete": false
-                                    }
-                                ],
-                                "explanation": "Here is an explanation for this excersize.",
-                                "question": "",
-                                "answer": "",
-                                "complete": false,
-                                "completed_on": ""
-                            },
-                            "total_to_complete": 0,
-                            "complete": false,
-                            "needs_approval": true,
-                            "approved_by": "",
-                            "approved_on": "",
-                            "approved": false
-                        },
-                    ]
-                }
-            ]
-        }
-    ]
-}
-*/
--- server/api/user/index.js
+++ server/api/user/index.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict';
-
-
+    //REQUIRES
     var express = require('express');
     var controller = require('./user.ctrl');
     var config = require('../../config/environment');
@@ -9,34 +8,26 @@
 
     var router = express.Router();
 
-    //think of '/api/user' +'xxx'
+    /////ENDPOINTS FOR USERS/////
 
-    router.get('/', auth.hasRole('admin'), controller.index);
-    router.delete('/:id', auth.hasRole('admin'), controller.destroy);
-    router.get('/me', auth.isAuthenticated(), controller.me);
+    router.get('/', auth.hasRole('admin'), controller.index);//GET ALL USERS FROM DATABASE
+    router.delete('/:id', auth.hasRole('admin'), controller.destroy);//DELETE SPECIFIC USER FROM DATABASE
+    router.get('/me', auth.isAuthenticated(), controller.me);//
     router.put('/:id/password', auth.isAuthenticated(), controller.changePassword);
     router.get('/:id', auth.isAuthenticated(), controller.show);
     router.post('/', controller.create);
-
-    //Mike
-    // router.get('/', controller.getAllUsers);
-    router.get('/userInfo/:memberId', controller.findByID);
-    router.get('/gym/:gymID', controller.getUsersByGym);
-    router.get('/details/:memberId', controller.getUserDetails);
-    router.get('-logos/:memberId', controller.getUserLogos);
-    router.get('-logos/physical', controller.getUserLogos);
-    router.get('-pathos/:memberId', controller.getUserPathos);
-    router.get('-ethos/:memberId', controller.getUserEthos);
-    router.put('/updateEval', controller.updateEvalStatus);
-    router.put('/updateAnswer', controller.updateAnswer);
-    router.put('/updateProgression', controller.updateProgression);
-
-    // router.post('-add', controller.addUser);
-    router.put('/update', controller.updateUser);
-    router.put('/update/isadmin', controller.userIsAdminUpdate);
-    router.put('/update/isactive', controller.userIsActiveUpdate);
-
-
+    router.get('/userInfo/:memberId', controller.findByID);//FIND SPECIFIC GYM
+    router.get('/gym/:gymID', controller.getUsersByGym);//GET ALL USERS FROM SPECIFIC GYM
+    router.get('/details/:memberId', controller.getUserDetails);//GET SPECIFIC USER DETAILS
+    router.get('/logos/:memberId', controller.getUserLogos);//GET LOGOS PATHWAY FOR SPECIFIC USER
+    router.get('/pathos/:memberId', controller.getUserPathos);//GET PATHOS PATHWAY FOR SPECIFIC USER
+    router.get('/ethos/:memberId', controller.getUserEthos);//GET ETHOS PATHWAY FOR SPECIFIC USER
+    router.put('/updateEval', controller.updateEvalStatus);//UPDATE EVALUATION WHEN CHANGES HAPPEN ON FRONT
+    router.put('/updateAnswer', controller.updateAnswer);//UPDATE ANSWERS FROM KNOWLEDGE QUESTIONS
+    router.put('/updateProgression', controller.updateProgression);//UPDATE OR ADD PROGRESSIONS FOR PHYSICAL ON FRONT
+    router.put('/update', controller.updateUser);//GENERAL UPDATE USER ENDPOINT
+    router.put('/update/isadmin', controller.userIsAdminUpdate);//UPDATE WHETHER USER IS ADMIN
+    router.put('/update/isactive', controller.userIsActiveUpdate);//UPDATE WHETHER USER IS ACTIVE
 
     module.exports = router;
 
