83798cb5bd468f6bfae49353ae7ec6204a5a7cc1
Brian Ng <bcn220@gmail.com>
2015-09-30 14:06:14 -0600

    show/hide modal based on if answer to q exists

--- client/app/user/main/mainCtrl.js
+++ client/app/user/main/mainCtrl.js
@@ -10,6 +10,21 @@ angular.module('app')
   // userObj for modal and main page
   $scope.userObject = userObj;
 
+  // checks to see if an answer exists to initial modal question and will show/hide accordingly
+  $(document).ready(function() {
+    if ($scope.userObject.get_to_know.answers.length === 0) {
+      $('#modal1').openModal();
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+      $('.modal-trigger').leanModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+        complete: function() {} // Callback for Modal close
+      });
+    }
+  });
+
   // get percentage data for stage circles
   $scope.logosPercent = Math.ceil($scope.getCurrentUser().pathways[0].completion.amount_completed);
 
@@ -20,7 +35,7 @@ angular.module('app')
   //disable button unless Logos is completed
   $scope.pathos = $scope.getCurrentUser().pathways[0].stages[1].complete;
 
-  // create new userObj to send thru submitModal function 
+  // create new userObj to send thru submitModal function
   var userObj = Auth.getCurrentUser();
   $scope.userInfo = {
     _id: userObj._id,
