16f5d72c4224d43cd91b7ee8d82375c9ca4c9da7
Brian DAmore <brian.damore@gmail.com>
2015-09-30 18:20:13 -0600

    Fixed gruntfile for dist
    removed unnecessary modules (ui.bootstrap)
    
    Signed-off-by: Brian DAmore <brian.damore@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -314,7 +314,7 @@ module.exports = function (grunt) {
           collapseWhitespace: true,
           removeAttributeQuotes: true,
           removeEmptyAttributes: true,
-          removeRedundantAttributes: true,
+          removeRedundantAttributes: false,
           removeScriptTypeAttributes: true,
           removeStyleLinkTypeAttributes: true
         },
--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,6 @@ var app = angular.module('app', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap',
   'ngAnimate',
   'ui.materialize'
 ]);
